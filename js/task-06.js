f1525e1b0a71aed298505f9c374d380607a11b3f
anniekrivega <121309759+AnnieKrivega@users.noreply.github.com>
2023-05-04 15:34:27 +0300

    add

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,7 @@
+const input = document.querySelector("#validation-input");
+const dataLength = Number(input.getAttribute("data-length"));
 
-    const inputEl = document.querySelector("#validation-input");
-    const dataLength = Number(input.getAttribute("data-length"));
-
-    input.addEventListener("blur", () => {
-    input.classList.toggle("valid", input.value.trim().length === dataLength);
-    input.classList.toggle("invalid", input.value.trim().length !== dataLength);
-    });
-    
+input.addEventListener("blur", () => {
+input.classList.toggle("valid", input.value.trim().length === dataLength);
+input.classList.toggle("invalid", input.value.trim().length !== dataLength);
+});
